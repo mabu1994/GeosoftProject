c79d806cd6545528ab61623a876e34beb0d346f5
Fmazin <fabian.fermazin@wwu.de>
2020-09-11 11:02:54 +0200

    Active User refined

--- currentUser/currentUser.js
+++ currentUser/currentUser.js
@@ -1,4 +1,10 @@
-
+/**
+ * changeUser - Eine Funktion, die einen Anufruf an den Server als Promise
+ * startet mit der Id eines Nutzerkontos, dass dann auf aktiv gesetzt wird.
+ *
+ * @param  {string} newUser Die Id des Kontos, dass auf aktiv gesetzt wird
+ * @return {Promise} Promise mit ajax Aufruf an den Server "/changeActive"
+ */
 function changeUser(newUser){
     console.log(newUser);
     return new Promise(function (res,rej){
@@ -10,6 +16,12 @@ function changeUser(newUser){
     });
 }
 
+/**
+ * getUser - Eine Funktion, die den aktuell aktiven User zurück gibt mithilfe
+ * eines Serveraufrufs.
+ *
+ * @return {Promise} Promise mit ajax Aufruf an den Server "/getActive"
+ */
 function getUser(){
   return new Promise(function (res,rej){
       $.ajax({
@@ -20,12 +32,23 @@ function getUser(){
   });
 }
 
+/**
+ * displayUser - Eine Funktion, die den aktuellen User in einem html Element
+ * mit der id "user" zeigt.
+ *
+ */
 async function displayUser(){
   var cUser = await getUser();
   console.log(cUser);
   $("#user").html("Sie sind angemeldet als: " + cUser[0]._id);
 }
 
+/**
+ * logoutUser - Eine Funktion, die einen Anufruf an den Server als Promise
+ * startet, dass das aktive Konto auf inaktiv gesetzt wird.
+ *
+ * @return {Promise} Promise mit ajax Aufruf an den Server "/logoutActive"
+ */
 function logoutUser(){
   return new Promise(function (res,rej){
       $.ajax({
@@ -36,6 +59,11 @@ function logoutUser(){
   });
 }
 
+/**
+ * backToLogin - description
+ *
+ * @return {type}  description
+ */
 async function backToLogin(){
   var logout = await logoutUser();
   window.open("/login", "_self");
